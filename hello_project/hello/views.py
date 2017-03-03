673bd082f36685a84624644431d45c97eeb11f6f
cucy <292016176@qq.com>
2017-03-03 17:07:12 +0800

    快速创建一个页面, 定义url app templates等用户列表页面

--- hello_project/hello/views.py
+++ hello_project/hello/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+def hello(request):
+    user_list = User.objects.all() # 从用户模型查看所有的用户
+    return render(request, 'table.html', {'user_list': user_list})
